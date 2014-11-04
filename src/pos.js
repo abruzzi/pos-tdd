fe35da4a237088b6653a3bf40ba00290b1e4e8c6
Qiu Juntao <juntao.qiu@gmail.com>
2014-11-05 01:29:50 +1100

    a more real oo version

--- src/pos.js
+++ src/pos.js
@@ -1,3 +1,96 @@
+function Item(name, count, unit, price, barcode) {
+	this.name = name;
+	this.count = count;
+	this.unit = unit;
+	this.price = price;
+	this.barcode = barcode;
+}
+
+Item.prototype.format = function() {
+	return "名称："+this.name+"，数量："+this.count+this.unit+"，单价："+this.price.toFixed(2)+"(元)，小计："+(this.calcItemPrice()).toFixed(2)+"(元)\n";
+}
+
+Item.prototype.calcItemPrice = function() {
+	return this.count * this.price;
+}
+
+function Barcode(code) {
+	var pair = code.split('-');
+	this.barcode = pair[0];
+	this.count = pair[1] ? pair[1] : 1;
+}
+
+function POS(items) {
+	this.items = items;
+	this.result = "";
+}
+
+POS.prototype.findByBarcode = function(barcode) {
+	for(var i = 0; i < this.items.length; i++) {
+		var current = this.items[i];
+		if(barcode == current.barcode) {
+			return new Item(current.name, 1, current.unit, current.price, current.barcode);
+		}
+	}
+
+	return null;
+}
+
+POS.indexOf = function(items, barcode) {
+	for(var i = 0; i < items.length; i++) {
+		if(items[i].barcode == barcode) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+POS.prototype.normalize = function() {
+	var items = [];
+
+	var that = this;
+	this.barcodes.forEach(function(barcode) {
+		var code = new Barcode(barcode);
+		var item = that.findByBarcode(code.barcode);
+
+		if(code.count) {
+			item.count = code.count;
+		}
+
+		var index = POS.indexOf(items, code.barcode);
+		if(index >= 0) {
+			items[index].count += 1;
+		} else {
+			items.push(item);
+		}
+	});
+
+	return items;
+}
+
+POS.prototype.scan = function(barcodes) {
+	this.barcodes = barcodes;
+	this.result += "***<没钱赚商店>购物清单***\n";
+
+	var sum = 0;
+	var items = this.normalize();
+
+	var that = this;
+	items.forEach(function(item) {
+		that.result += item.format();
+		sum += item.calcItemPrice();
+	});
+    
+    this.result += "----------------------\n";
+    this.result += "总计："+sum.toFixed(2)+"(元)\n" +
+			"**********************";
+}
+
+POS.prototype.print = function() {
+	return this.result;
+}
+
 var items = [
         {
             barcode: "ITEM000000",
@@ -37,86 +130,8 @@ var items = [
         }
     ];
 
-function Item(name, count, unit, price, barcode) {
-	this.name = name;
-	this.count = count;
-	this.unit = unit;
-	this.price = price;
-	this.barcode = barcode;
-}
-
-Item.prototype.format = function() {
-	return "名称："+this.name+"，数量："+this.count+this.unit+"，单价："+this.price.toFixed(2)+"(元)，小计："+(this.calcItemPrice()).toFixed(2)+"(元)\n";
-}
-
-Item.prototype.calcItemPrice = function() {
-	return this.count * this.price;
-}
-
-function findByBarcode(barcode) {
-	for(var i = 0; i < items.length; i++) {
-		var current = items[i];
-		if(barcode == current.barcode) {
-			return new Item(current.name, 1, current.unit, current.price, current.barcode);
-		}
-	}
-
-	return null;
-}
-
-function indexOf(items, barcode) {
-	for(var i = 0; i < items.length; i++) {
-		if(items[i].barcode == barcode) {
-			return i;
-		}
-	}
-
-	return -1;
-}
-
-function Barcode(code) {
-	var pair = code.split('-');
-	this.barcode = pair[0];
-	this.count = pair[1] ? pair[1] : 1;
-}
-
-function normalize(barcodes) {
-	var items = [];
-
-	barcodes.forEach(function(barcode) {
-		var code = new Barcode(barcode);
-		var item = findByBarcode(code.barcode);
-
-		if(code.count) {
-			item.count = code.count;
-		}
-
-		var index = indexOf(items, code.barcode);
-		if(index >= 0) {
-			items[index].count += 1;
-		} else {
-			items.push(item);
-		}
-	});
-
-	return items;
-}
-
 function format(barcodes) {
-	var result = "";
-	result += "***<没钱赚商店>购物清单***\n";
-
-	var sum = 0;
-	var items = normalize(barcodes);
-
-	items.forEach(function(item) {
-		result += item.format();
-		sum += item.calcItemPrice();
-	});
-    
-    result += "----------------------\n";
-    result += "总计："+sum.toFixed(2)+"(元)\n" +
-			"**********************";
-
-	return result;
+	var pos = new POS(items);
+	pos.scan(barcodes);
+	return pos.print();
 }
