6e69fd4da6e9c4d9d9dae985a887101769854aed
Qiu Juntao <juntao.qiu@gmail.com>
2014-11-05 00:20:17 +1100

    add mixed tests for coverage

--- spec/pos-spec.js
+++ spec/pos-spec.js
@@ -89,5 +89,19 @@ describe("POS", function() {
 		var result = format(["ITEM000002-3"]);
 
 		expect(result).toBe(expected);            
-	})
+	});
+
+	it("should scan items mixed together", function() {
+		var expected = 
+			"***<没钱赚商店>购物清单***\n" +
+            "名称：苹果，数量：3斤，单价：5.50(元)，小计：16.50(元)\n" +
+            "名称：可口可乐，数量：2瓶，单价：3.00(元)，小计：6.00(元)\n" +
+    		"----------------------\n" +
+            "总计：22.50(元)\n" +
+            "**********************";
+
+		var result = format(["ITEM000002-3", "ITEM000000", "ITEM000000"]);
+
+		expect(result).toBe(expected);            
+	});
 })
